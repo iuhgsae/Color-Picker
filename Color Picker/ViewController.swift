7cb7f06fa68f07473cc7a286a8dd8efd09737bff
Hana Sakamoto <sarcrates@sarcratess-MacBook-Pro.local>
2017-05-04 20:28:47 -0700

    Update to iOS 10

--- Color Picker/ViewController.swift
+++ Color Picker/ViewController.swift
@@ -9,6 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var RedSlider: UISlider!
+    @IBOutlet weak var GreenSlider: UISlider!
+    @IBOutlet weak var BlueSlider: UISlider!
+    
+    @IBOutlet weak var RedLabel: UILabel!
+    @IBOutlet weak var GreenLabel: UILabel!
+    @IBOutlet weak var BlueLabel: UILabel!
+    @IBOutlet weak var ColorLabel: UILabel!
+    
+    @IBOutlet weak var ResetButton: UIButton!
+    
+    
+    
+    var redColor : Float = 0
+    var greenColor : Float = 0
+    var blueColor : Float = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +37,70 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func RedSliderAction(_ sender: UISlider) {
+    
+        changeColors()
+    }
+    
+    @IBAction func GreenSliderAction(_ sender: UISlider) {
+    
+        changeColors()
+    }
+    
+    @IBAction func BlueSliderAction(_ sender: UISlider) {
+        changeColors()
+    
+    }
+    
+    func ChangeLabelColor(){
+        ColorLabel.backgroundColor = UIColor(red: CGFloat(redColor), green: CGFloat(greenColor), blue: CGFloat(blueColor), alpha: 1.0)
+        changeLabels()
+    }
 
+    
+    func changeColors(){
+        
+        redColor = RedSlider.value
+        greenColor = GreenSlider.value
+        blueColor = BlueSlider.value
+        
+        ChangeLabelColor()
+        
+    }
+    
+    
+    func changeLabels(){
+        
+        let RoundedRed = String(format: "%0.0f", (redColor * 255))
+        let RoundedGreen = String(format: "%0.0f", (greenColor * 255))
+        let RoundedBlue = String(format: "%0.0f", (blueColor * 255))
+        
+        
+        RedLabel.text = "Red: \(RoundedRed)"
+        GreenLabel.text = "Green: \(RoundedGreen)"
+        BlueLabel.text = "Blue: \(RoundedBlue)"
+        
+        
+    }
+    
+    @IBAction func ResetButtonAction(_ sender: UIButton) {
+        ResettingSliders()
+        
+        
+    }
+    
+    func ResettingSliders(){
+        RedSlider.value = 0.5
+        GreenSlider.value = 0.5
+        BlueSlider.value = 0.5
+        
+        RedLabel.text = String("Red: 0")
+        GreenLabel.text = String("Green: 0")
+        BlueLabel.text = String("Blue: 0")
 
+        
+    }
+    
 }
 
