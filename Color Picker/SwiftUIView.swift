f5ff8972e9d48d991ee46de148584e1002251cbf
me <none@none.com>
2022-11-11 02:52:27 -0700

    jump 10 - 16

---
+++ Color Picker/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  Color Picker
+//
+//  Created by sasha on 11/11/22.
+//  Copyright © 2022 sarcrates. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
